79201592554a409a8652d940441eab82ff46f7f5
Ilia <107078455+IliaIvashkevich@users.noreply.github.com>
2022-09-16 12:42:43 +0000

    Deleted Exeption

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
 
